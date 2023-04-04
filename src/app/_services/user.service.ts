887304d141803b6ccb7b72debeac3e5ef5cc6232
omurthy <omuralcinthy@gmail.com>
2023-04-04 16:33:13 +0300

    changes url

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost/api/test/';
+const API_URL = 'https://authopher-server.vercel.app/api/test/';
 
 @Injectable({
   providedIn: 'root',
